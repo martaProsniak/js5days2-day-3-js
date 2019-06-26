1ea3681002c56c0741b4fa6e43fcd8aa5316039f
martaprosniak <martaprosniak@gmail.com>
2019-06-26 14:12:02 +0200

    createCar changed to displayCar

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const carElement = document.createElement('div')
-
-carElement.style.width = '100px'
-carElement.style.height = '100px'
-carElement.style.backgroundColor = 'red'
-
-carElement.style.position = 'absolute'
-carElement.style.left = '0px'
-
 const body = document.querySelector('body')
-body.appendChild(carElement)
-
 let position = 0
 
+
+
+function displayCar() {
+    carElement.style.width = '100px'
+    carElement.style.height = '100px'
+    carElement.style.backgroundColor = 'red'
+
+    carElement.style.position = 'absolute'
+    carElement.style.left = '0px'
+
+    body.appendChild(carElement)
+}
+
+function move() {
+    position += 10
+    carElement.style.left = position + 'px'
+}
+
+displayCar();
+
 setInterval(
-    // declaration of anonymous function
-    function () {
-        position += 10
-        carElement.style.left = position + 'px'
-    },
+    // move instead of move() to pass the whole function into setInterval function
+    move,
     100
 )
 
