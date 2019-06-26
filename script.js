0ca9928a78ae11a5c1f7e87119fceebd6563cb50
martaprosniak <martaprosniak@gmail.com>
2019-06-26 13:52:56 +0200

    Car is moving

--- script.js
+++ script.js
@@ -4,5 +4,20 @@ carElement.style.width = '100px'
 carElement.style.height = '100px'
 carElement.style.backgroundColor = 'red'
 
+carElement.style.position = 'absolute'
+carElement.style.left = '0px'
+
 const body = document.querySelector('body')
 body.appendChild(carElement)
+
+let position = 0
+
+setInterval(
+    // declaration of anonymous function
+    function () {
+        position += 10
+        carElement.style.left = position + 'px'
+    },
+    100
+)
+
