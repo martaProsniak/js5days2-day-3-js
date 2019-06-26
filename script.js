0d0104e091695f0049ff66668a4414590119b274
martaprosniak <martaprosniak@gmail.com>
2019-06-26 12:57:09 +0200

    Car element created

---
+++ script.js
@@ -0,0 +1,8 @@
+const carElement = document.createElement('div')
+
+carElement.style.width = '100px'
+carElement.style.height = '100px'
+carElement.style.backgroundColor = 'red'
+
+const body = document.querySelector('body')
+body.appendChild(carElement)
