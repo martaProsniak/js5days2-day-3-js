4e43604c339edf3c3b3bb21656b3f29224eef423
martaprosniak <martaprosniak@gmail.com>
2019-06-26 11:56:30 +0200

    Some simple mathematic functions

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return 'Enter valid number!'
+    }
+
     return x + y;
+}
+
+function multiply(a, b){
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'Enter valid number!'
+    }
+    return a * b;
+}
+
+function square (a) {
+    return multiply(a, a);
 }
